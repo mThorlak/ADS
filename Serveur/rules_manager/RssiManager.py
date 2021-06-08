0d61a0fcff17f9cde9be8f929af097521871125c
mThorlak <thomas.foch@protonmail.com>
2021-06-08 23:05:00 +0200

    Euclidean distance => done, determine location in progress

--- Serveur/rules_manager/RssiManager.py
+++ Serveur/rules_manager/RssiManager.py
@@ -49,20 +49,71 @@ def sortLogsInTheGoodTimeRange(macAddress, date):
     return result
 
 
-# Get all sensors with room description and range that received signal from the specified mac_address at the same time
+# Get all sensors that received signal from the specified mac_address at the same time,
+# and calculate the distance between us
+# Here is a list example : {'Test3-01': 1, 'Test8-02': 59, 'Test4-05': 49}
+# The first one is ever the best RSSI
 def calculateEuclideanDistance(listOfLogsSorted):
-    # => Récupérer les logs qui sont à la même minute, à quelques secondes prêt
-    # Comparer la puissance du RSSI qu'ils ont récupérés
-    # Distance euclidienne, avec le plus fort RSS pour valeur 1
+    # Get the best RSSI
+    bestRSSI = -500
+    indexOfBestRSSI = 0
+    i = 0
+    for logs in listOfLogsSorted:
+        rssiLogs = logs[3]
+        if rssiLogs > bestRSSI:
+            bestRSSI = rssiLogs
+            indexOfBestRSSI = i
+        i = i+1
+    # Now we calculate the distance between each logs and store its in a list
+    listDistance = {listOfLogsSorted[indexOfBestRSSI][0]: 1}
+    i = 0
+    while i < len(listOfLogsSorted):
+        if i == indexOfBestRSSI:
+            i = i + 1
+            continue
+        distance = (listOfLogsSorted[i][3] * -1) - 1
+        listDistance[listOfLogsSorted[i][0]] = distance
+        i = i + 1
+    return listDistance
+
+
+# TODO : to finish
+# {'Test3-01': ['Test3-01', '2020-13-10T21:38:20Z', 'A1:B5:R1:N1:B9', 1, 'Garage'],
+# 'Test8-02': ['Test8-02', '2020-13-10T21:39:10Z', 'A1:B5:R1:N1:B9', 59, 'Cuisine'],
+# 'Test4-05': ['Test4-05', '2020-13-10T21:39:03Z', 'A1:B5:R1:N1:B9', 49, 'Salon']}
+def determineLocation(listDistance, date):
+    print(listDistance)
+    # Get the good all logs file
+    date = date[:-1]
+    dateConverter = datetime.strptime(date, '%Y-%d-%mT%H:%M:%S')
+    dateOfTheDay = dateConverter.date().strftime("%d-%m-%Y")
+    pathFile = ARCHIVE_LOG_PATH + dateOfTheDay + '/all_logs.csv'
+    allLogsFileModel = salfm.SensorAllLogsFileModel(pathFile)
+    print(allLogsFileModel.content)
+    sensorConfigModel = scm.SensorConfigModel()
+    result = {}
+    for i in listDistance:
+        log = allLogsFileModel.content.loc[allLogsFileModel.content['ID'] == str(i)].values.tolist()
+        log = log[0]
+        log[3] = listDistance[i]
+        for nameSensor in sensorConfigModel.content["Name"].values:
+            if str(nameSensor).capitalize() in log[0]:
+                sensorConfig = sensorConfigModel.content.loc[sensorConfigModel.content["Name"] == nameSensor]\
+                    .values.tolist()
+                roomDescription = sensorConfig[0][3]
+                log.append(roomDescription)
+        result[i] = log
+    print(result)
+    print(sensorConfigModel.content["Name"].values)
     return None
 
 
-# Get all sensors with room description and range that received signal from the specified mac_address at the same time
-# def function...
+# TODO : sort list of logs sorted with the good range
 
 def test():
     listOfLogsSorted = sortLogsInTheGoodTimeRange("A1:B5:R1:N1:B9", "2020-10-11T21:38:44Z")
-    # calculateEuclideanDistance(listOfLogsSorted)
+    listDistance = calculateEuclideanDistance(listOfLogsSorted)
+    determineLocation(listDistance, "2020-10-11T21:38:44Z")
     return None
 
 
